75e27a1cda3b0399963fdb53424f1f85735356d7
gurulahima <name_not@live.com>
2018-02-05 00:36:20 +0100

    made changes to physics, size, and speed of player
    Sized down small circles and gave them a bit of drag just in case.
    Made them follow player and big circles without much fluterring about.
    Player can now have up o 6 circles no problem, and hey follow him
    without jittering or blinks.
    Also created a default_level which will be used as a starting point for
    new levels.

--- Assets/Scripts/CircleBehaviour.cs
+++ Assets/Scripts/CircleBehaviour.cs
@@ -23,15 +23,28 @@ public class CircleBehaviour : MonoBehaviour {
 
     void FixedUpdate(){
     	//fixing the exiting circles, standard size of 0.85
-    	for (int i = 0; i < transform.GetChild(0).childCount; i++)
+/*    	for (int i = 0; i < transform.GetChild(0).childCount; i++)
     	{
     	    float dist = Vector3.Distance(transform.GetChild(0).GetChild(i).transform.position, transform.position);
     	    // if (dist > 0.85f)
     	    // {
     	        transform.GetChild(0).GetChild(i).transform.position = Vector3.MoveTowards(transform.position, transform.GetChild(0).GetChild(i).transform.position, Time.deltaTime * 8);
     	    // }
-    	}
+    	}*/
     	// widthOfVibe = Random.Range(1f, Mathf.Sin(Time.time)*2);
     	//restoring if any circle scapes boundary
+    			//fixing the exiting circles, standard size of 0.85
+		for (int i = 0; i < transform.GetChild(0).childCount; i++)
+		{
+		    // float dist = Vector3.Distance(transform.GetChild(0).GetChild(i).transform.position, transform.position);
+		    // Vector3 direction = (transform.position - transform.GetChild(0).GetChild(i).transform.position);
+		    // direction = direction.normalized;
+		    //Debug.Log("the distance is ===== " + dist);
+		    // if (dist > 0.85f)
+		    {
+		        transform.GetChild(0).GetChild(i).transform.position = Vector3.Lerp(transform.position, transform.GetChild(0).GetChild(i).transform.position, Time.deltaTime);
+		        // transform.GetChild(0).GetChild(i).GetComponent<Rigidbody2D>().AddForce(direction * 2, ForceMode2D.Force);
+		    }
+		}
     }
 }
